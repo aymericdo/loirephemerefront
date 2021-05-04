061c4fad75138bcca04c467de15f355b762fc8f2
RaphaelWski <raphael.wojciechowski2@gmail.com>
2021-05-04 21:24:21 +0200

    configure project structure & add NgZorro (erreur import)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { NgZorroModule } from './shared/ngzorro.module';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { fr_FR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(fr);
 
 @NgModule({
   declarations: [
@@ -10,9 +21,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgZorroModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    SharedModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: fr_FR }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+/* Modules */
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NgZorroModule } from './ngzorro.module';
+
+/* Components */
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    NgZorroModule,
+    RouterModule
+  ],
+  exports: [
+    NavComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
