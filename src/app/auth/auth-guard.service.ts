7e7726072fafac9d3c5488f7d71ad938063391bf
aymeric dominique <aymeric.dominique@gmail.com>
2023-01-14 19:00:22 +0100

    cascade

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { filter, take } from 'rxjs';
-import { Restaurant } from 'src/app/interfaces/restaurant.interface';
 import { Access } from 'src/app/interfaces/user.interface';
 import { selectUser, selectUserRestaurants } from 'src/app/modules/login/store/login.selectors';
 import { AppState } from 'src/app/store/app.state';
@@ -27,31 +26,31 @@ export class AuthGuardService implements CanActivate {
         this.router.navigate(['page', '404']);
       }
 
-      let userRestaurants: Restaurant[] = [];
       this.store.select(selectUserRestaurants).pipe(filter(Boolean), take(1))
         .subscribe((restaurants) => {
-          userRestaurants = restaurants;
+          const userRestaurants = restaurants;
+
+          if (!userRestaurants.some((resto) => resto.code === code)) {
+            this.router.navigate(['page', '404']);
+          }
+
+          const restaurantId = userRestaurants.find((restaurant) => restaurant.code === code)?.id!;
+
+          this.store.select(selectUser).pipe(filter(Boolean), take(1))
+            .subscribe((user) => {
+              const userAccess: { [restaurantId: string]: Access[] } =
+                user.access as { [restaurantId: string]: Access[] };
+
+              if (!userAccess[restaurantId].includes(route.data.access)) {
+                if (userAccess[restaurantId].length) {
+                  this.router.navigate([code, 'admin', userAccess[restaurantId][0]]);
+                } else {
+                  this.router.navigate(['page', '404']);
+                }
+              }
+            });
         });
 
-      if (!userRestaurants.some((resto) => resto.code === code)) {
-        this.router.navigate(['page', '404']);
-      }
-
-      const restaurantId = userRestaurants.find((restaurant) => restaurant.code === code)?.id!;
-
-      let userAccess: { [restaurantId: string]: Access[] } = {};
-      this.store.select(selectUser).pipe(filter(Boolean), take(1))
-        .subscribe((user) => {
-          userAccess = user.access as { [restaurantId: string]: Access[] };
-        });
-
-      if (!userAccess[restaurantId].includes(route.data.access)) {
-        if (userAccess[restaurantId].length) {
-          this.router.navigate([code, 'admin', userAccess[restaurantId][0]]);
-        } else {
-          this.router.navigate(['page', '404']);
-        }
-      }
     }
     return true;
   }
