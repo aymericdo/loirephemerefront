3105eed64999af0463c64384e5165adf4bf9c4f4
aymeric dominique <aymeric.dominique@gmail.com>
2023-07-09 03:04:23 +0200

    refacto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   protocolHttp: 'https://',
   protocolWs: 'wss://',
-  apiWs: 'localhost:3000',
-  api: 'localhost:3000',
+  apiWs: 'backend.aymericdo.ovh/wsloirephemereback',
+  api: 'backend.aymericdo.ovh/loirephemereback',
 };
 
 /*
