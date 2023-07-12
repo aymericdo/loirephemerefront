094baf5931ef70b804e8a7aa84c92f0ecaea20a8
aymeric dominique <aymeric.dominique@gmail.com>
2023-07-12 02:13:40 +0200

    remove console log

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -128,7 +128,6 @@ export class NavComponent implements OnInit, OnDestroy {
 
 
         this.routeWithoutNavBar = data?.routeWithoutNavBar || false;
-        console.log(this.routeWithoutNavBar);
         this.routeName = data?.routeName || null;
 
         this.siderCollapseChange(this.isSmallScreen() ? isSiderCollapsed : false);
