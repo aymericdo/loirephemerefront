e48e90703ed66fdfa62473ceee19f0d6ab103730
Aymeric Dominique <aymeric.dominique@gmail.com>
2025-01-30 16:06:12 +0100

    update

--- src/app/modules/login/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/modules/login/components/confirmation-modal/confirmation-modal.component.ts
@@ -1,8 +1,6 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { NzInputNumberComponent } from 'ng-zorro-antd/input-number';
-import { Observable, ReplaySubject, takeUntil } from 'rxjs';
+import { Observable } from 'rxjs';
 import { selectLoading } from 'src/app/modules/login/store/login.selectors';
 import { AppState } from 'src/app/store/app.state';
 
@@ -12,85 +10,28 @@ import { AppState } from 'src/app/store/app.state';
     styleUrls: ['./confirmation-modal.component.scss'],
     standalone: false
 })
-export class ConfirmationModalComponent implements OnInit, OnDestroy {
+export class ConfirmationModalComponent implements OnInit {
   @Input() submitButton = $localize`Suivant`;
   @Output() clickConfirm = new EventEmitter<{ emailCode: string }>();
 
-  @ViewChild('figure1') figure1Element!: NzInputNumberComponent;
-  @ViewChild('figure2') figure2Element!: NzInputNumberComponent;
-  @ViewChild('figure3') figure3Element!: NzInputNumberComponent;
-  @ViewChild('figure4') figure4Element!: NzInputNumberComponent;
-
   isLoading$!: Observable<boolean>;
-  validateForm!: UntypedFormGroup;
+  value: string = '';
 
-  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
-
-  constructor(private store: Store<AppState>, private fb: UntypedFormBuilder) { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
     this.isLoading$ = this.store.select(selectLoading);
+  }
 
-    this.validateForm = this.fb.group({
-      figure1: [null, [Validators.required, Validators.minLength(0), Validators.maxLength(9)]],
-      figure2: [null, [Validators.required, Validators.minLength(0), Validators.maxLength(9)]],
-      figure3: [null, [Validators.required, Validators.minLength(0), Validators.maxLength(9)]],
-      figure4: [null, [Validators.required, Validators.minLength(0), Validators.maxLength(9)]],
-    });
+  formatter: (value: string) => string = value => value.replace(/\D/g,'').toUpperCase();
 
-    this.isLoading$
-      .pipe(
-        takeUntil(this.destroyed$)
-      ).subscribe((loading) => {
-        if (loading) {
-          this.validateForm.disable();
-        } else {
-          this.validateForm.enable();
-        }
+  handleSubmit(): void {
+    if (this.value.replace(/\D/g,'').toUpperCase().length === 4) {
+      this.clickConfirm.emit({
+        emailCode: this.value.replace(/\D/g,'').toUpperCase(),
       });
-  }
-
-  ngOnDestroy() {
-    this.destroyed$.next(true);
-    this.destroyed$.complete();
-  }
-
-  onPaste(event: ClipboardEvent) {
-    event.preventDefault();
-    const clipboardData = event.clipboardData;
-    const pastedText = clipboardData!.getData('text');
-    if (pastedText && pastedText.toString().length === 4 && pastedText.match(/\d{4}/g)?.length) {
-      this.validateForm.controls.figure1.setValue(pastedText[0]);
-      this.validateForm.controls.figure2.setValue(pastedText[1]);
-      this.validateForm.controls.figure3.setValue(pastedText[2]);
-      this.validateForm.controls.figure4.setValue(pastedText[3]);
+    } else {
+      this.value = '';
     }
-  }
-
-  goNext(event: Event, figure: number): void {
-    if (!(event as InputEvent).data) return;
-    switch (figure) {
-      case 1: {
-        this.figure1Element.focus();
-        break;
-      }
-      case 2: {
-        this.figure2Element.focus();
-        break;
-      }
-      case 3: {
-        this.figure3Element.focus();
-        break;
-      }
-      case 4: {
-        this.figure4Element.focus();
-        break;
-      }
-    }
-  }
-
-  submitForm(): void {
-    const emailCode = `${this.validateForm.value.figure1}${this.validateForm.value.figure2}${this.validateForm.value.figure3}${this.validateForm.value.figure4}`;
-    this.clickConfirm.emit({ emailCode });
   }
 }
